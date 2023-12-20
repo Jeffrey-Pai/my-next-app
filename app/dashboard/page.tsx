4dbf056459daa40d98a530a86c772b9d517ba421
Jeffrey-Hying <jeffrey@hying.com.tw>
2023-12-20 17:51:48 +0800

    add stock api(simple)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,28 @@
-// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
-export default function Page() {
-  return <h1>Hello, Dashboard Page!</h1>
+async function getData() {
+  const res = await fetch(
+    'https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch=tse_2330.tw'
+  )
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Page() {
+  const data = await getData()
+  const stockId = data.msgArray[0].c
+  const stockName = data.msgArray[0].n
+  return (
+    <main>
+      <div>
+        <p>{stockId}</p>
+        <p>{stockName}</p>
+      </div>
+    </main>
+  )
 }
