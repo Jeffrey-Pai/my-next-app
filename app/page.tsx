17ef0b830ee5acecf8ea228efae9c9b9b2810771
Jeffrey-Pai <s940112jeffrey@gmail.com.tw>
2023-12-21 17:15:39 +0800

    add counter in component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Link from 'next/link'
+import Counter from '../component/counter'
 
 export default function Page() {
   return (
     <div>
       <h1>Home</h1>
-      <Link href="/dashboard">股票儀錶板</Link>
+      <Link
+        href="/dashboard"
+        className="block mb-2 text-blue-500 hover:underline"
+      >
+        股票儀錶板
+      </Link>
+      <Counter></Counter>
     </div>
   )
 }
