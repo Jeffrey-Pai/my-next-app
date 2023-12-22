d85d54da2ce095024d90457cf43168fc88d55663
Jeffrey-Pai <s940112jeffrey@gmail.com>
2023-12-23 00:06:12 +0800

    add submit btn to fix bug

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,36 +8,45 @@ export default async function IndexPage({
 }) {
   const search = searchParams.q ?? '';
   let stockId, stockName, stockHigh, stockLow, stockVol;
-  if(search!= ''){
+  if (search !== '') {
     const res = await fetch(
       `https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch=tse_${search}.tw`
     );
-    const data = await res.json()
-   stockId = data.msgArray[0].c
-   stockName = data.msgArray[0].n
-   stockHigh = data.msgArray[0].h
-   stockLow = data.msgArray[0].l
-   stockVol = data.msgArray[0].v
-  }
-  else {
-     stockId = '';
-     stockName = '';
-     stockHigh = '';
-     stockLow = '';
-     stockVol = '';
+    const data = await res.json();
+  
+    if (data.msgArray && data.msgArray.length > 0) {
+      stockId = data.msgArray[0].c;
+      stockName = data.msgArray[0].n;
+      stockHigh = data.msgArray[0].h;
+      stockLow = data.msgArray[0].l;
+      stockVol = data.msgArray[0].v;
+    } else {
+      stockId = '';
+      stockName = '';
+      stockHigh = '';
+      stockLow = '';
+      stockVol = '';
+    }
+  } else {
+    stockId = '';
+    stockName = '';
+    stockHigh = '';
+    stockLow = '';
+    stockVol = '';
   }
   
   
 
   return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">      
       <Search />
+      <div className='mt-3'>
         <p>股票代號:{stockId}</p>
         <p>股票名稱:{stockName}</p>
         <p>最高值:{stockHigh}</p>
         <p>最低值:{stockLow}</p>
         <p>成交量:{stockVol}</p>
+      </div>       
     </main>
   );
 }
