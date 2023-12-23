c6c3522d468d1cb542c23059b0d29646e7113574
Jeffrey-Pai <s940112jeffrey@gmail.com>
2023-12-23 10:05:08 +0800

    add otc condition

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -7,25 +7,41 @@ export default async function IndexPage({
   searchParams: { q: string };
 }) {
   const search = searchParams.q ?? '';
-  let stockId, stockName, stockHigh, stockLow, stockVol;
+  let stockId, stockName, stockHigh, stockLow, stockVol, res ,data;
   if (search !== '') {
-    const res = await fetch(
+    res = await fetch(
       `https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch=tse_${search}.tw`
     );
-    const data = await res.json();
+    data = await res.json();
   
-    if (data.msgArray && data.msgArray.length > 0) {
+    if (data.msgArray && data.msgArray.length > 0) 
+    {      
       stockId = data.msgArray[0].c;
       stockName = data.msgArray[0].n;
       stockHigh = data.msgArray[0].h;
       stockLow = data.msgArray[0].l;
       stockVol = data.msgArray[0].v;
-    } else {
-      stockId = '';
-      stockName = '';
-      stockHigh = '';
-      stockLow = '';
-      stockVol = '';
+    } 
+    else {
+      if (search !== '') 
+      {
+      res = await fetch(
+        `https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch=otc_${search}.tw`
+      )
+      data = await res.json(); 
+      stockId = data.msgArray[0].c;
+      stockName = data.msgArray[0].n;
+      stockHigh = data.msgArray[0].h;
+      stockLow = data.msgArray[0].l;
+      stockVol = data.msgArray[0].v;     
+      }
+      else {
+        stockId = '';
+        stockName = '';
+        stockHigh = '';
+        stockLow = '';
+        stockVol = '';
+      }      
     }
   } else {
     stockId = '';
